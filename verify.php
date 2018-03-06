c72b88f046f37c2737f068faedee8c183a1f2f2c
dozzdiy <37081528+dozzdiy@users.noreply.github.com>
2018-03-06 10:24:24 +0700

    .

--- verify.php
+++ verify.php
@@ -1,4 +1,6 @@
 <?php
+$proxy = 'http://velodrome.usefixie.com:80';
+$proxyauth = 'fixie:sq025rsPlndXgXu';
 
 $access_token = '976f/MsdT5+5rqsngTjGaLwTTy5sDD5Elzo4Q/k9RtHoiufsYFd2qWuHkKHp+XxeidqM0FOx7DKBdj2uryxrd2ZOvgiLu3YTukYUDgc60MhqEQAoJA5RNPbXz4xslAkUryvxZR+hovIaPY/XNY4LOAdB04t89/1O/w1cDnyilFU=';
 
@@ -10,6 +12,8 @@ $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt($ch, CURLOPT_PROXY, $proxy);
+curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
 $result = curl_exec($ch);
 curl_close($ch);
 
