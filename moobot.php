74a98e5836ff328dbd0bf76468fbd17b3b7f0396
dozzdiy <37081528+dozzdiy@users.noreply.github.com>
2018-04-09 16:37:55 +0700

    Update moobot.php

--- moobot.php
+++ moobot.php
@@ -1,41 +1,19 @@
 <?php
-//header('Content-Type: text/html; charset=UTF-8');
 
-//$url = "http://webdev10g.nhso.go.th/ihr/lm/FrontEnd/LineMsg";
-//$url = "http://66.23.242.79/api.php";
 $url = "http://ihr.nhso.go.th/lm/FrontEnd/LineMsg";
 
 $message = curlExecuteGet($url);
-//echo $message . "<br/>";
 
-/*$tab = array("UTF-8", "ASCII", "Windows-1252", "ISO-8859-15", "ISO-8859-1", "ISO-8859-6", "CP1256", "windows-874", "tis-620"); 
-$chain = ""; 
-foreach ($tab as $i) 
-	{ 
-		foreach ($tab as $j) 
-		{ 
-			$chain .= " $i$j ".iconv($i, $j, "$message") . "<br/>";
-		} 
-	} 
-echo $chain;*/
-
-//$message = iconv("windows-874", "UTF-8", $message);
 $message = "\n" . trim(str_replace(array("\n","\\n"),array("","\n"), $message));
 
 if(!stristr($message, "weekend") and !stristr($message, "holiday")){
 	sendlinemesg();
 	$res = notify_message($message);
-}else{
-	//echo "e";
 }
 
 function sendlinemesg() {
-    //$token = 'GRlw90niM9BbRj7Gzi0mJ7he8nNTud2vfPPwLMnBYkI'; //intranet
-    //$token = 'qE3NHid8jvzP0BWLvAmNXpwF66HWIs7uL5Ck8fJHxbG'; //intra test
-    //$token = 'vG1VEFyWpU0wZN75XNYUSslmlPbNQdtezmSlw9alyaP'; //beakfast
     $token = 'Ga0sPA6J6XGSxrYAgP4IqYRvpyiC7gcDarSbfh5Ha2c'; //it nhso
-    
-	define('LINE_API', "https://notify-api.line.me/api/notify");
+    define('LINE_API', "https://notify-api.line.me/api/notify");
     define('LINE_TOKEN', $token);
 }
 
