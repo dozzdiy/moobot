f1ecc22bf691326ac482da15b30eaba0dca4633c
dozzdiy <37081528+dozzdiy@users.noreply.github.com>
2018-07-24 14:14:49 +0700

    .
    .

---
+++ moobot3.php
@@ -0,0 +1,44 @@
+<?php
+
+$message = "ÊÇÑÊ´ÕªÒÇ Êº¤";
+
+	sendlinemesg();
+	$res = notify_message($message);
+
+function sendlinemesg() {
+    $token = 'xrYwtPB6YLQazTTAW0DpDahsOvRMCCMCz10yEgdsNhn'; //it nhso
+    define('LINE_API', "https://notify-api.line.me/api/notify");
+    define('LINE_TOKEN', $token);
+}
+
+function notify_message($message) {
+    $queryData = array('message' => $message);
+    $queryData = http_build_query($queryData, '', '&');
+    //echo $queryData;
+    $headerOptions = array(
+        'http' => array(
+            'method' => 'POST',
+            'header' => "Content-Type: application/x-www-form-urlencoded\r\n"
+            . "Authorization: Bearer " . LINE_TOKEN . "\r\n"
+            . "Content-Length: " . strlen($queryData) . "\r\n",
+            'content' => $queryData
+        )
+    );
+    $context = stream_context_create($headerOptions);
+    $result = file_get_contents(LINE_API, FALSE, $context);
+    $res = json_decode($result);
+    return $res;
+}
+
+function curlExecuteGet($url) {
+    $ch = curl_init();
+    $timeout = 0;
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+    $file_contents = curl_exec($ch);
+    curl_close($ch);
+    return $file_contents;
+}
+
+?>
